66f95664ae4640d176601af6dd8498f52fcd12cf
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-10-18 16:48:48 +0200

    ADD: exercise on the palindrome word

--- exercise-palindrome/palindrome.js
+++ exercise-palindrome/palindrome.js
@@ -1,1 +1,16 @@
-console.log("ciao");
+//creo una variabile parola per il prompt
+//creo la funzione che confronta la parola originale con la sua inversa
+//creo un if che stampa la risposta se è palindroma o no
+
+let userWord = prompt("Inserisci una parola");
+
+function isPalindrome(word) {
+    let reversedWord = word.split('').reverse().join('');
+    return word === reversedWord;
+}
+
+if (isPalindrome(userWord) === true) {
+    console.log("La parola è palindroma");
+} else {
+    console.log("La parola non è palindroma");
+}
